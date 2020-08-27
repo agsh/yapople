b7de93e271dc9bcbebe37cd895b39847b7950820
Andrew D.Laptev <a.d.laptev@gmail.com>
2020-08-27 20:45:38 +0300

    Jest config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    collectCoverageFrom: [
+        'lib/**/*.js',
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',testMatch: [
+        '**/__tests__/**/*.test.js',
+    ],
+};
