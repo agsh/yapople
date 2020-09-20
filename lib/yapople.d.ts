af3881f347aa3703153623f913e89677886ca1fe
Liore Shai <liore.shai@gmail.com>
2020-09-20 10:09:28 -0400

    add typescript bindings - Resolves: #13

---
+++ lib/yapople.d.ts
@@ -0,0 +1,38 @@
+interface YapopleClientConfig {
+    host: string;
+    port:  number;
+    tls?: boolean;
+    mailparser?: boolean;
+    username: string;
+    password: string;
+    options?: any;
+}
+
+interface Message {
+    html: string;
+    headers: any;
+    subject: string;
+    references: string[];
+    messageId: string;
+    inReplyTo: string[];
+    priority: string;
+    from: object[];
+    replyTo: object[];
+    to: object[];
+    date: Date;
+    receivedDate: Date;
+}   
+
+export class Client {
+    constructor(config: YapopleClientConfig)
+    connect(config?:YapopleClientConfig): Promise<void>;
+    count(): Promise<number>;
+    retrieve(messageNumber:number|number[]):Promise<Message[]>
+    retrieveAll(): Promise<Message[]>;
+    delete(messageNumber:number|number[]):Promise<Message[]>
+    deleteAll(): Promise<any>;
+    retrieveAndDeleteAll(): Promise<Message[]>;
+    list(messageNumber:number):Promise<any>;
+    quit():Promise<void>;
+    disconnect():Promise<void>
+}
