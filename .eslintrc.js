fe9f65ef244905bf2aad8a74b6d2c3c5893030fe
Andrew D.Laptev <a.d.laptev@gmail.com>
2024-05-17 16:24:09 +0300

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,18 +12,10 @@ module.exports = {
         SharedArrayBuffer: 'readonly',
     },
     parserOptions: {
-        ecmaFeatures: { jsx: true },
         ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: [
-        'import'
-    ],
     rules: {
-        'import/no-unresolved': [2, { commonjs: true, amd: true }],
-        'import/named': 2,
-        'import/namespace': 2,
-        'import/default': 2,
         'linebreak-style': ['error', 'unix'],
         'quotes': ['error', 'single'],
         'semi': ['error', 'always'],
